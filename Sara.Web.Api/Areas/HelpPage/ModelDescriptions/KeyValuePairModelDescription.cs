0ff64b2733eaddae4d7fe2a00ba69033ac50dc92
Fábio Salomão Silva <Fbio Salomo Silva@DESKTOP-NCI3HCS>
2017-07-29 15:17:31 -0500

    Commit da tarde do dia 29-07

---
+++ Sara.Web.Api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Sara.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Sara.Web.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Sara.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
